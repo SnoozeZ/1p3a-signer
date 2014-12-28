28c9ae50580d4aed464588131d45e3e6bacbd4ed
Snooze <zhangweiqi357@gmail.com>
2014-12-29 00:54:00 +0800

    multi streads

--- 1p3a-signer.py
+++ 1p3a-signer.py
@@ -3,6 +3,7 @@
 import requests
 import re
 import time
+import thread
 ####
 username = "snooze"
 password = "7efd42209ac34abcb13a07069cd88fe4"  #password in 32-md5 here
@@ -24,7 +25,6 @@ def getTime():
   return {'h':hour, 'm':minute, 's':second}
 
 def login():
-  my_info = []
   #get cookie
   login_data = {'username': username, 
     'password': password, 
@@ -38,40 +38,40 @@ def login():
     except Exception,ex:
       print Exception,":",ex
       time.sleep(1)
-  my_info.append(r.cookies)
+  my_cookies = r.cookies
   
   #find the odd WTF "formhash"
   while True:
     try:
       r = requests.post("http://www.1point3acres.com/bbs/",
-        cookies = my_info[0])
+        cookies = my_cookies)
       break
     except Exception,ex:
       print Exception,":",ex
       time.sleep(1)    
   pattern = 'formhash=.*?&'
   formhash = re.findall(pattern,r.content,re.S)[0][9:17]
-  my_info.append(formhash)
+  my_info = (my_cookies, formhash)
   
   return my_info
 
   
-def sign(my_info):
+def sign(cookies, formhash):
   #SIGN NOW!
-  sign_data = {'formhash': my_info[1], 
+  sign_data = {'formhash': formhash, 
     'qdxq': 'fd', 
     'qdmode': '1', 
     'todaysay': blabla,
     'fastreply': '0'}
-  while True:
-    try:
-      r = requests.post("http://www.1point3acres.com/bbs/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&sign_as=1&inajax=1",
-        data = sign_data,
-        cookies = my_info[0])
-      break
-    except Exception,ex:
-      print Exception,":",ex
-      time.sleep(1)
+  #while True:
+  try:
+    r = requests.post("http://www.1point3acres.com/bbs/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&sign_as=1&inajax=1",
+      data = sign_data,
+      cookies = cookies)
+    #break
+  except Exception,ex:
+    print Exception,":",ex
+    #time.sleep(1)
       
   if r.content.find("ÂÜ²·") != -1:
     print 'Successfully signed! -3-'
@@ -89,11 +89,13 @@ if __name__ == "__main__":
     else:
       my_info = login()
       slptm = (59 - tmp['m']) * 60 + (59 - tmp['s'])
-      print "It's comming... I will sleep " +str(slptm-10)+ " seconds."
-      time.sleep(slptm - 10)
-      for i in range(100):
+      print "It's comming... I will sleep " +str(slptm-4)+ " seconds."
+      time.sleep(slptm-4)
+      for i in range(40):
         print "Try " +str(i)+" times, your local time is " + str(time.strftime('%H:%M:%S',time.localtime(time.time())))
-        if sign(my_info):
-          continue
-        else:
-          break
+        thread.start_new_thread(sign, my_info)
+        time.sleep(0.2) 
+        #if sign(my_info):
+        #  continue
+        #else:
+        #  break
